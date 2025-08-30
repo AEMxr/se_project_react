bba8b13f0c90228cfce7378000a1447c77f60adf
AEMxr <asmalcuit@outlook.com>
2025-08-29 19:13:01 -0700

    toggleswitch pathing

--- src/components/ToggleSwitch/ToggleSwitch.jsx
+++ src/components/ToggleSwitch/ToggleSwitch.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import "./ToggleSwitch.css";
+import "../ToggleSwitch/ToggleSwitch.css";
 import CurrentTemperatureUnitContext from "../../contexts/CurrentTemperatureUnitContext";
 
 const Toggle_Switch = ({ isOn: isOnProp, handleToggle: handleToggleProp }) => {
